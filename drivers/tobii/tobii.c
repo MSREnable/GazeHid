68c386a173b369fb79b7c55d9eee66b87a2d8068
Harish Kulkarni <harish.kulkarni@microsoft.com>
2017-04-03 16:52:44 -0700

    Updated Tobii driver to latest stream engine sdk

--- drivers/tobii/tobii.c
+++ drivers/tobii/tobii.c
@@ -7,18 +7,16 @@
 // * Eye positions in a different callback than gaze point
 // * The discovery protocol cannot be based on tcp urls. We shouldnt rely on opening the first found device either
 // * What about implementing a head tracker now that stream sdk has support for it
-// * Calibration requires a license. This is incompatible with standardization
 
 
-#define TOBII_USE_STREAM_SDK
-
-#ifdef TOBII_USE_STREAM_SDK
 #include "tobii/tobii.h"
 #include "tobii/tobii_streams.h"
 
 typedef struct _SENSOR_CONTEXT
 {
-    tobii_context_t*    TobiiContext;
+    tobii_api_t*        TobiiApi;
+    tobii_device_t*     TobiiDevice;
+    BOOL                GazeStreamSubscribed;
     BOOL                DeviceStarted;
     HANDLE              ThreadHandle;
     DWORD               ThreadId;
@@ -28,19 +26,23 @@ typedef struct _SENSOR_CONTEXT
 
 #define CLEANUP_ON_FAIL(error) if (error != TOBII_ERROR_NO_ERROR) goto Cleanup;
 
-void OnGazeEvent(float x, float y, int64_t timestamp_us, void* userData)
+void OnGazeEvent(tobii_gaze_point_t const* gazePoint, void* userData)
 {
     PDEVICE_CONTEXT deviceContext = (PDEVICE_CONTEXT)userData;
     GAZE_REPORT gazeReport;
 
-    KdPrint(("OnGazeEvent: Timestamp=0x%p x=%e y=%e\n", timestamp_us, x, y));
+    KdPrint(("OnGazeEvent: Timestamp=0x%p x=%e y=%e, valid=%d\n", 
+        gazePoint->timestamp_us, gazePoint->position_xy[0], gazePoint->position_xy[1], gazePoint->validity));
 
-    gazeReport.ReportId = HID_USAGE_TRACKING_DATA;
-    gazeReport.TimeStamp = timestamp_us;
-    gazeReport.GazePoint.X = x;
-    gazeReport.GazePoint.Y = y;
-    gazeReport.ReportId = HID_USAGE_TRACKING_DATA;
-    SendGazeReport(deviceContext, &gazeReport);
+    if (gazePoint->validity == TOBII_VALIDITY_VALID)
+    {
+        gazeReport.ReportId = HID_USAGE_TRACKING_DATA;
+        gazeReport.TimeStamp = gazePoint->timestamp_us;
+        gazeReport.GazePoint.X = gazePoint->position_xy[0];
+        gazeReport.GazePoint.Y = gazePoint->position_xy[1];
+        gazeReport.ReportId = HID_USAGE_TRACKING_DATA;
+        SendGazeReport(deviceContext, &gazeReport);
+    }
 }
 
 DWORD WINAPI TobiiFrameProc(PVOID startParam)
@@ -53,14 +55,14 @@ DWORD WINAPI TobiiFrameProc(PVOID startParam)
 
     while (!sensorContext->ExitThread)
     {
-        error = tobii_wait_for_callbacks(sensorContext->TobiiContext);
+        error = tobii_wait_for_callbacks(sensorContext->TobiiDevice);
         if (error == TOBII_ERROR_NO_ERROR)
         {
-            tobii_process_callbacks(sensorContext->TobiiContext);
+            tobii_process_callbacks(sensorContext->TobiiDevice);
         }
         else if (error == TOBII_ERROR_CONNECTION_FAILED)
         {
-            error = tobii_reconnect(sensorContext->TobiiContext);
+            error = tobii_reconnect(sensorContext->TobiiDevice);
             KdPrint(("Connection failed. Reconnect attempt returned error: 0x%0x\n", error));
         }
         else
@@ -81,25 +83,26 @@ BOOL InitializeEyeTracker(PDEVICE_CONTEXT deviceContext)
     }
 
     deviceContext->EyeTracker = sensorContext;
+    memset(sensorContext, 0, sizeof(*sensorContext));
 
+    tobii_api_t* api;
+    tobii_device_t *device;
     tobii_error_t error;
-    tobii_context_t * context;
 
-    sensorContext->ExitThread = FALSE;
-
-    // To be able to use the API you need to initialize it once. This is your chance to provide custom handling for memory allocation and logging.
-    error = tobii_initialize(NULL, NULL);
+    error = tobii_api_create(&api, NULL, NULL);
     CLEANUP_ON_FAIL(error);
+
+    sensorContext->TobiiApi = api;
 
     // Sending an empty url connects to the first eye tracker found.
-    error = tobii_context_create(&context, 0);
+    error = tobii_device_create(api, NULL, &device);
     CLEANUP_ON_FAIL(error);
-
-    sensorContext->TobiiContext = context;
+    sensorContext->TobiiDevice = device;
 
     // Subscribe to a stream of wearable eye tracking data. The callback you supply will be called from tobii_process_callbacks.
-    error = tobii_gaze_point_subscribe(context, OnGazeEvent, deviceContext);
+    error = tobii_gaze_point_subscribe(device, OnGazeEvent, deviceContext);
     CLEANUP_ON_FAIL(error);
+    sensorContext->GazeStreamSubscribed = TRUE;
 
     // Create a thread for data processing
     sensorContext->ThreadHandle = CreateThread(NULL, 0, TobiiFrameProc, deviceContext, CREATE_SUSPENDED, &sensorContext->ThreadId);
@@ -119,108 +122,29 @@ void ShutdownEyeTracker(PDEVICE_CONTEXT deviceContext)
     PSENSOR_CONTEXT sensorContext = (PSENSOR_CONTEXT)deviceContext->EyeTracker;
     
     sensorContext->ExitThread = TRUE;
-    WaitForSingleObject(sensorContext->ThreadHandle, INFINITE);
+    if (sensorContext->ThreadHandle)
+    {
+        WaitForSingleObject(sensorContext->ThreadHandle, INFINITE);
+        sensorContext->ThreadHandle = NULL;
+    }
 
-    tobii_gaze_point_unsubscribe(sensorContext->TobiiContext);
-    tobii_context_destroy(sensorContext->TobiiContext);
-    tobii_terminate();
+    if (sensorContext->GazeStreamSubscribed)
+    {
+        tobii_gaze_point_unsubscribe(sensorContext->TobiiDevice);
+        sensorContext->GazeStreamSubscribed = FALSE;
+    }
+
+    if (sensorContext->TobiiDevice)
+    {
+        tobii_device_destroy(sensorContext->TobiiDevice);
+        sensorContext->TobiiDevice = NULL;
+    }
+    
+    if (sensorContext->TobiiApi)
+    {
+        tobii_api_destroy(sensorContext->TobiiApi);
+        sensorContext->TobiiApi = NULL;
+    }
     
     deviceContext->EyeTracker = NULL;
 }
-
-
-#else
-#include "tobiigaze.h"
-#include "tobiigaze_discovery.h"
-#include "tobiigaze_error_codes.h"
-
-#define CLEANUP_ON_FAIL(error) if (error != TOBIIGAZE_ERROR_SUCCESS) goto Cleanup;
-
-
-void OnEventLoopExit(tobiigaze_error_code error, void *userData)
-{
-    UNREFERENCED_PARAMETER(error);
-    UNREFERENCED_PARAMETER(userData);
-}
-
-void OnError(tobiigaze_error_code error, void *userData)
-{
-    UNREFERENCED_PARAMETER(error);
-    UNREFERENCED_PARAMETER(userData);
-
-}
-
-void OnGazeEvent(const struct tobiigaze_gaze_data *gazeData, const struct tobiigaze_gaze_data_extensions *gazeExtensions, void *userData)
-{
-    UNREFERENCED_PARAMETER(gazeExtensions);
-    UNREFERENCED_PARAMETER(userData);
-
-    PDEVICE_CONTEXT deviceContext = (PDEVICE_CONTEXT)userData;
-    const struct tobiigaze_gaze_data_eye *eye = NULL;
-
-    if (gazeData->tracking_status == TOBIIGAZE_TRACKING_STATUS_BOTH_EYES_TRACKED ||
-        gazeData->tracking_status == TOBIIGAZE_TRACKING_STATUS_ONLY_LEFT_EYE_TRACKED ||
-        gazeData->tracking_status == TOBIIGAZE_TRACKING_STATUS_ONE_EYE_TRACKED_PROBABLY_LEFT)
-    {
-        eye = &gazeData->left;
-    }
-
-    if (gazeData->tracking_status == TOBIIGAZE_TRACKING_STATUS_BOTH_EYES_TRACKED ||
-        gazeData->tracking_status == TOBIIGAZE_TRACKING_STATUS_ONLY_RIGHT_EYE_TRACKED ||
-        gazeData->tracking_status == TOBIIGAZE_TRACKING_STATUS_ONE_EYE_TRACKED_PROBABLY_RIGHT)
-    {
-        eye = &gazeData->right;
-    }
-
-    if (eye == NULL)
-    {
-        return;
-    }
-
-    GAZE_REPORT gazeReport;
-    gazeReport.ReportId = HID_USAGE_TRACKING_DATA;
-    gazeReport.TimeStamp = gazeData->timestamp;
-    gazeReport.GazePoint.X = (float)eye->gaze_point_on_display_normalized.x;
-    gazeReport.GazePoint.Y = (float)eye->gaze_point_on_display_normalized.y;
-    SendGazeReport(deviceContext, &gazeReport);
-}
-
-BOOL InitializeEyeTracker(PDEVICE_CONTEXT deviceContext)
-{
-    char url[MAX_PATH];
-    tobiigaze_error_code error;
-
-    tobiigaze_get_connected_eye_tracker(url, MAX_PATH, &error);
-    CLEANUP_ON_FAIL(error);
-
-    deviceContext->EyeTracker = tobiigaze_create(url, &error);
-    CLEANUP_ON_FAIL(error);
-
-    tobiigaze_register_error_callback((tobiigaze_eye_tracker *)deviceContext->EyeTracker, OnError, deviceContext);
-
-    tobiigaze_run_event_loop_on_internal_thread(deviceContext->EyeTracker, OnEventLoopExit, deviceContext);
-
-    tobiigaze_connect(deviceContext->EyeTracker, &error);
-    CLEANUP_ON_FAIL(error);
-
-    tobiigaze_start_tracking(deviceContext->EyeTracker, OnGazeEvent, &error, deviceContext);
-    CLEANUP_ON_FAIL(error);
-
-Cleanup:
-    return error == TOBIIGAZE_ERROR_SUCCESS;
-}
-
-
-void ShutdownEyeTracker(PDEVICE_CONTEXT deviceContext)
-{
-    tobiigaze_error_code error;
-    tobiigaze_stop_tracking(deviceContext->EyeTracker, &error);
-
-    tobiigaze_disconnect(deviceContext->EyeTracker);
-
-    tobiigaze_break_event_loop(deviceContext->EyeTracker);
-
-    tobiigaze_destroy(deviceContext->EyeTracker);
-}
-
-#endif
